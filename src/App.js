aabd0f8c413afdd92725ec133e2d4aa4bcbba13a
codergirlshecodes <acacchiola180@gmail.com>
2024-03-16 01:47:07 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Alexa Cacchiola and is {" "}
@@ -11,6 +12,7 @@ export default function App() {
       target="_blank" rel="noreferrer"> open-sourced on GitHub
       </a>
     </footer>
+    </div>
     </div>
   );
 }
